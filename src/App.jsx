f3fb54fa2781d7b308801f36d357c7055843969f
Thura Freelance <mozet89@gmail.com>
2024-04-22 15:41:30 +0800

    create header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2> Order Your Favorite YZK Food Here</h2>
+        <p>
+          Choose from diverse featuring a delectable array of dishes cracked
+          with the food delivery
+        </p>
+        <button type="button">View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
